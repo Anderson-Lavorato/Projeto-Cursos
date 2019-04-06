4cb69abbefed43e33ae29461c85ef7337eef3b60
Anderson-Lavorato <anderson.lavoratoo@gmail.com>
2019-04-06 12:17:45 -0300

    Adicionado validade

--- validarCompra.php
+++ validarCompra.php
@@ -6,6 +6,57 @@
     $cvv = $_REQUEST["cvv"];
     $nomeCurso = $_REQUEST["nomeCurso"];
     $precoCurso = $_REQUEST["precoCurso"];
+    $erros = [];
+
+    // Funções
+
+    function validarNome ($nome) {
+        return strlen($nome) > 0 && strlen($nome) <= 15;
+    }
+
+    function validarCpf($CPF) {
+        return strlen($CPF) == 11;
+    }
+
+    function validarNroCartao($nroCartao){
+        $primeiraNumero = substr($nroCartao, 0, 1);
+        return $primeiraNumero == 4 || $primeiraNumero == 5 || $primeiraNumero == 6;
+    }
+
+    function validarData($data) {
+        $dataAtual = date("Y-m");
+        return $data >= $dataAtual;
+    }
+
+    function validarCvv($cvv) {
+        return strlen($cvv) == 3;
+    }
+    
+    function validarCompra($nome, $CPF, $nroCartao, $data, $cvv){
+        global $erros;
+
+        if (!validarNome($nome)){
+            array_push($erros, "Preencha seu nome");
+        }
+
+        if(!validarCpf($CPF)){
+            array_push($erros, "Seu CPF precisa ter 11 caracteres");
+        }
+
+        if(!validarNroCartao($nroCartao)){
+            array_push($erros, "Seu cartão precisa começar com 4, 5 ou 6");
+        }
+
+        if(!validarData($data)){
+            array_push($erros, "A validade precisa ser maior que a data atual");
+        }
+
+        if(!validarCvv($cvv)){
+            array_push($erros, "Seu cvv precisa ter 3 caracteres");
+        }
+    }
+
+    validarCompra($nome, $CPF, $nroCartao, $validade, $cvv);
 ?>
 
 <!DOCTYPE html>
@@ -29,21 +80,40 @@
 <body>
     <div class="container">
         <div class="col-md-6 col-md-offset-3" >
-            <div class="panel panel-primary">
-                <div class="panel-heading">
-                    <span>Compra realizada com sucesso!</span>
+            <?php if (count($erros) > 0) : ?>
+                <div class="panel panel-danger">
+                    <div class="panel-heading">
+                        <span>Preencha seus dados corretamente!</span>
+                    </div>
+                    <div class="pane-body">
+                        <ul class="list-group">
+                            <?php foreach ($erros as $chaves => $valorErro) : ?>
+                                <li class="list-group-item">
+                                    <?= $valorErro; ?>
+                                </li>
+                            <?php endforeach; ?>
+                        </ul>
+                        <div class="center">
+                            <a href="index.php">Voltar para home</a></div>
+                    </div>
                 </div>
-                <div class="pane-body">
-                    <ul class="list-group">
-                        <li class="list-group-item"><strong> Nome Curso: </strong><?php echo $nomeCurso; ?></li>
-                        <li class="list-group-item"><strong> Preço: R$ </strong> <?php echo $precoCurso; ?></li>
-                        <li class="list-group-item"><strong> Nome Completo: </strong><?= $nome; ?></li>
-                    </ul>
-                    <div class="center">
-                        <a href="index.php">Voltar para home</a></div>
+            <?php else: ?>
+                 <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <span>Compra realizada com sucesso!</span>
+                        </div>
+                        <div class="pane-body">
+                            <ul class="list-group">
+                                <li class="list-group-item"><strong> Nome Curso: </strong><?php echo $nomeCurso; ?></li>
+                                <li class="list-group-item"><strong> Preço: R$ </strong> <?php echo $precoCurso; ?></li>
+                                <li class="list-group-item"><strong> Nome Completo: </strong><?= $nome; ?></li>
+                            </ul>
+                            <div class="center">
+                                <a href="index.php">Voltar para home</a></div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+            <?php endif ; ?>
     </div>
 </body>
 </html>
